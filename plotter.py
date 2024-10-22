05874aa686ba8498e2932fa0fea7140bf1ca4fba
Davide Domini <davide.domini@unibo.it>
2024-10-22 17:14:06 +0200

    feat: add plotter

---
+++ plotter.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import matplotlib
+import glob
+import re
+
+
+def extractVariableNames(filename):
+    """
+    Gets the variable names from the Alchemist data files header.
+
+    Parameters
+    ----------
+    filename : str
+        path to the target file
+
+    Returns
+    -------
+    list of list
+        A matrix with the values of the csv file
+
+    """
+    with open(filename, 'r') as file:
+        dataBegin = re.compile(r'\d')
+        lastHeaderLine = ''
+        for line in file:
+            if dataBegin.match(line[0]):
+                break
+            else:
+                lastHeaderLine = line
+        if lastHeaderLine:
+            regex = re.compile(r' (?P<varName>\S+)')
+            return regex.findall(lastHeaderLine)
+        return []
+
+def openCsv(path):
+    """
+    Converts an Alchemist export file into a list of lists representing the matrix of values.
+
+    Parameters
+    ----------
+    path : str
+        path to the target file
+
+    Returns
+    -------
+    list of list
+        A matrix with the values of the csv file
+
+    """
+    regex = re.compile(r'\d')
+    with open(path, 'r') as file:
+        lines = filter(lambda x: regex.match(x[0]), file.readlines())
+        return [[float(x) for x in line.split()] for line in lines]
+
+def load_data_from_csv(path, round):
+    files = glob.glob(f'{path}experiment*globalRound-{round}.csv')
+    dataframes = []
+    print(f'For round {round} loaded {len(files)} files')
+    for file in files:
+        columns = extractVariableNames(file)
+        print(columns)
+        data = openCsv(file)
+        df = pd.DataFrame(data, columns=columns)
+        dataframes.append(df)
+    return dataframes
+
+def plot(mean, std, round):
+    metrics = ['reward[min]', 'reward[max]', 'reward[mean]',
+               'batteryPercentage[min]', 'batteryPercentage[max]', 'batteryPercentage[mean]',
+               'localComponentsPercentage[min]', 'localComponentsPercentage[max]', 'localComponentsPercentage[mean]',
+               ]
+    for metric in metrics:
+        plt.plot(mean['time'], mean[metric], color='#440154')
+        plt.fill_between(mean['time'], mean[metric] - std[metric], mean[metric] + std[metric], color='#440154', alpha=0.2)
+        plt.title(f'Global Round {round}')
+        plt.xlabel('Time')
+        plt.ylabel(metric)
+        plt.savefig(f'charts/globalRound-{round}_{metric}.pdf')
+        plt.close()
+
+if __name__ == '__main__':
+
+    charts_dir = 'charts/'
+    Path(charts_dir).mkdir(parents=True, exist_ok=True)
+    data_path = 'data/'
+
+    for round in range(1, 11):
+        dataframes = load_data_from_csv(data_path, round)
+        df_concat = pd.concat(dataframes)
+        by_row_index = df_concat.groupby(df_concat.index)
+        mean = by_row_index.mean()
+        mean = mean.dropna()
+        var = by_row_index.var()
+        var = var.dropna()
+        plot(mean, var, round)
