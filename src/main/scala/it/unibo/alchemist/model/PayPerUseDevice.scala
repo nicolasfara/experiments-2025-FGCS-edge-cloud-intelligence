dbea26e15104cef05d84bdbd9a88b963abd7d9ad
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-10-23 10:11:09 +0200

    feat: implement cost model for infrastructural devices

---
+++ src/main/scala/it/unibo/alchemist/model/PayPerUseDevice.scala
@@ -0,0 +1,43 @@
+package it.unibo.alchemist.model
+
+import it.unibo.alchemist.model.actions.AbstractLocalAction
+import it.unibo.alchemist.model.implementations.actions.RunSurrogateScafiProgram
+import it.unibo.alchemist.model.molecules.SimpleMolecule
+
+import scala.jdk.CollectionConverters.CollectionHasAsScala
+
+class PayPerUseDevice[T, P <: Position[P]](
+    private val environment: Environment[T, P],
+    private val node: Node[T],
+    private val dollarsPerHourPerComponent: Double,
+) extends AbstractLocalAction[T](node) {
+  private lazy val surrogateRunner = node.getReactions.asScala
+    .flatMap(_.getActions.asScala)
+    .find(_.isInstanceOf[RunSurrogateScafiProgram[T, P]])
+    .map(_.asInstanceOf[RunSurrogateScafiProgram[T, P]])
+    .getOrElse(throw new IllegalStateException(s"No `RunSurrogateScafiProgram` found for node ${node.getId}"))
+  private var previousTime: Option[Double] = None
+  private var totalCost = 0.0
+
+  override def cloneAction(node: Node[T], reaction: Reaction[T]): Action[T] = ???
+
+  override def execute(): Unit = {
+    val currentTime = environment.getSimulation.getTime.toDouble
+    previousTime match {
+      case None => previousTime = Some(currentTime)
+      case Some(prevTime) =>
+        val deltaTime = currentTime - prevTime
+        previousTime = Some(currentTime)
+        val cost = deltaTime * dollarsPerHourPerComponent / 3600
+        val costLastDelta = surrogateRunner.isSurrogateFor.size * cost
+        totalCost += costLastDelta
+        node.setConcentration(PayPerUseDevice.COST_LAST_DELTA, costLastDelta.asInstanceOf[T])
+        node.setConcentration(PayPerUseDevice.TOTAL_COST, totalCost.asInstanceOf[T])
+    }
+  }
+}
+
+private object PayPerUseDevice {
+  private val COST_LAST_DELTA = new SimpleMolecule("costLastDelta")
+  private val TOTAL_COST = new SimpleMolecule("totalCost")
+}
