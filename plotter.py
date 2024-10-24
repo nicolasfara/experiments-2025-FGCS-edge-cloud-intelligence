23954d347e5e8bde0af042c50ebdfd84061ddffc
Davide Domini <davide.domini@unibo.it>
2024-10-24 13:26:09 +0200

    add plotting of multiple experiments

--- plotter.py
+++ plotter.py
@@ -55,8 +55,8 @@ def openCsv(path):
         lines = filter(lambda x: regex.match(x[0]), file.readlines())
         return [[float(x) for x in line.split()] for line in lines]
 
-def load_data_from_csv(path, round):
-    files = glob.glob(f'{path}experiment*globalRound-{round}.csv')
+def load_data_from_csv(path, experiment, round):
+    files = glob.glob(f'{path}experiment-{experiment}*globalRound-{round}.csv')
     dataframes = []
     print(f'For round {round} loaded {len(files)} files')
     for file in files:
@@ -66,32 +66,40 @@ def load_data_from_csv(path, round):
         dataframes.append(df)
     return dataframes
 
-def plot(mean, std, round):
-    metrics = ['reward[min]', 'reward[max]', 'reward[mean]',
-               'batteryPercentage[min]', 'batteryPercentage[max]', 'batteryPercentage[mean]',
-               'localComponentsPercentage[min]', 'localComponentsPercentage[max]', 'localComponentsPercentage[mean]',
-               ]
+def plot(mean, std, round, experiment, metrics):
     for metric in metrics:
         plt.plot(mean['time'], mean[metric], color='#440154')
         plt.fill_between(mean['time'], mean[metric] - std[metric], mean[metric] + std[metric], color='#440154', alpha=0.2)
         plt.title(f'Global Round {round}')
         plt.xlabel('Time')
         plt.ylabel(metric)
-        plt.savefig(f'charts/globalRound-{round}_{metric}.pdf')
+        plt.savefig(f'charts/{experiment}/globalRound-{round}_{metric}.pdf')
         plt.close()
 
 if __name__ == '__main__':
-
+    experiments = ['battery', 'costs']
     charts_dir = 'charts/'
     Path(charts_dir).mkdir(parents=True, exist_ok=True)
     data_path = 'data/'
 
-    for round in range(1, 11):
-        dataframes = load_data_from_csv(data_path, round)
-        df_concat = pd.concat(dataframes)
-        by_row_index = df_concat.groupby(df_concat.index)
-        mean = by_row_index.mean()
-        mean = mean.dropna()
-        var = by_row_index.var()
-        var = var.dropna()
-        plot(mean, var, round)
+    metrics = {
+        "battery": ['reward[min]', 'reward[max]', 'reward[mean]',
+                    'batteryPercentage[min]', 'batteryPercentage[max]', 'batteryPercentage[mean]',
+                    'localComponentsPercentage[min]', 'localComponentsPercentage[max]', 'localComponentsPercentage[mean]',
+                    ],
+        "costs": ['reward[min]', 'reward[max]', 'reward[mean]',
+                    'localComponentsPercentage[min]', 'localComponentsPercentage[max]', 'localComponentsPercentage[mean]',]
+    } 
+
+    for experiment in experiments: 
+        Path(f'{charts_dir}{experiment}/').mkdir(parents=True, exist_ok=True)
+
+        for round in range(1, 11):
+            dataframes = load_data_from_csv(data_path, experiment, round)
+            df_concat = pd.concat(dataframes)
+            by_row_index = df_concat.groupby(df_concat.index)
+            mean = by_row_index.mean()
+            mean = mean.dropna()
+            var = by_row_index.var()
+            var = var.dropna()
+            plot(mean, var, round, experiment, metrics[experiment])
