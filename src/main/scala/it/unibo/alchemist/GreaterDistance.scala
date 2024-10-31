7f41979d33f58550f08a41f96bff1975d76d7ebb
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-10-31 12:07:14 +0100

    feat: use grater distance to define a boundary

--- src/main/scala/it/unibo/alchemist/GreaterDistance.scala
+++ src/main/scala/it/unibo/alchemist/GreaterDistance.scala
@@ -3,8 +3,8 @@ package it.unibo.alchemist
 class GreaterDistance extends MyAggregateProgram {
 
   override def main(): Any = {
-    val distanceFromSource = inputFromComponent("it.unibo.alchemist.Gradient", Double.PositiveInfinity)
-    val result = distanceFromSource > 10.0
+    val distanceFromSource = inputFromComponent("it.unibo.alchemist.DensityEstimation", Double.PositiveInfinity)
+    val result = distanceFromSource > 6.0
     writeEnv("distance", result)
     result
   }
